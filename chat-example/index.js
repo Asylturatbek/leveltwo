4b63184ff4c149ea9d16488614e0bc9113578eab
Asylturatbek <asylturatbek@gmail.com>
2020-02-29 15:57:48 +0600

    socket app very easy

--- chat-example/index.js
+++ chat-example/index.js
@@ -1,10 +1,23 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello World</h1>')
+	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+	console.log('a user connected')
+	
+	socket.on('chat message', function(mesfromclie){
+		io.emit('chat message', mesfromclie)
+	})
+
+	socket.on('disconnect', function(){
+		console.log('user disconnected')
+	})
 })
 
 http.listen(PORT, function(){
