cd4b679d2864289476c32f4d8d4cc2db865b0ee0
Asylturatbek <asylturatbek@gmail.com>
2020-03-27 17:41:31 +0600

    wrapped up server part of project

--- vue_express/server/index.js
+++ vue_express/server/index.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
+const posts = require('./routes/api/posts.js')
+
+app.use('/api/posts', posts)
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
